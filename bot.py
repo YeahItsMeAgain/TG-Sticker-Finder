43707da1ef35dbbbbd3d499bbb371c02bcaa668f
Mevaser <mevaser.rotner@gmail.com>
2023-09-24 16:25:25 +0300

    Updated python-telegram-bot

--- bot.py
+++ bot.py
@@ -1,37 +1,26 @@
 from uuid import uuid4
 
-from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import (
-    Updater,
-    Filters,
-    CallbackContext,
-    InlineQueryHandler,
-    CommandHandler,
-)
+from loguru import logger
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
+from telegram.ext import Application, CommandHandler, InlineQueryHandler, filters
 
 from config import config
-from loguru import logger
-
 from stickers.providers import find_sticker_packs
 
 
 class Bot:
     def __init__(self):
-        self.updater = Updater(token=config.bot.token, use_context=True)
+        self.application = Application.builder().token(config.bot.token).build()
 
     def _add_handlers(self):
-        self.updater.dispatcher.add_handler(
-            InlineQueryHandler(self._sticker_finder_handler, run_async=True)
-        )
-
-        self.updater.dispatcher.add_handler(
+        self.application.add_handler(InlineQueryHandler(self._sticker_finder_handler))
+        self.application.add_handler(
             CommandHandler(
                 "start",
-                lambda update, _: update.message.reply_text("Hi! oh great master"),
-                Filters.chat(username=config.bot.allowed_usernames)
+                self._start,
+                filters.Chat(username=config.bot.allowed_usernames)
                 if config.bot.allowed_usernames
                 else None,
-                run_async=True,
             )
         )
 
@@ -40,15 +29,17 @@ class Bot:
         self._add_handlers()
 
         logger.info("Starting bot")
-        self.updater.start_polling()
-        self.updater.idle()
+        self.application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-    def _sticker_finder_handler(self, update: Update, _: CallbackContext):
+    async def _start(self, update: Update, *args, **kwargs):
+        await update.message.reply_text("Hi! oh great master")
+
+    async def _sticker_finder_handler(self, update: Update, *args, **kwargs):
         if (
             config.bot.allowed_usernames
             and update.effective_user.username not in config.bot.allowed_usernames
         ):
-            update.inline_query.answer(
+            await update.inline_query.answer(
                 [
                     InlineQueryResultArticle(
                         id=uuid4(),
@@ -67,7 +58,7 @@ class Bot:
             InlineQueryResultArticle(
                 id=uuid4(),
                 title=sticker_pack.name,
-                thumb_url=sticker_pack.image_url,
+                thumbnail_url=sticker_pack.image_url,
                 input_message_content=InputTextMessageContent(sticker_pack.link),
                 description=sticker_pack.description,
                 url=sticker_pack.link,
@@ -78,4 +69,4 @@ class Bot:
             f"{update.effective_user.username} is searching for {query}, received {len(results)} results"
         )
 
-        update.inline_query.answer(results)
+        await update.inline_query.answer(results)
