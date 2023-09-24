4b65ae31d59adfb8cd0aeaeb3e5e0045b9f6bf6f
Mevaser <mevaser.rotner@gmail.com>
2023-09-24 16:43:49 +0300

    Removed StickerProvider from config

--- config.py
+++ config.py
@@ -10,14 +10,8 @@ class Bot:
     allowed_usernames: List[str]
 
 @dataclass
-class StickerProvider:
-    name: str
-    link: str
-    query_body: Optional[str]
-
-@dataclass
 class Config:
     bot: Bot
 
-with open("config.json") as json_data_file:
+with open("config.json", encoding="ascii") as json_data_file:
     config = from_dict(data_class=Config, data=json.load(json_data_file))
